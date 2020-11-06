3b612586606e3fab83f39df5bab07075592c105e
Vlad <vlad_chekmarev_01@mail.ru>
2020-11-06 23:24:24 +0300

    Add xaml Edit/Add Client

--- OOODamage/Views/ListClients.xaml.cs
+++ OOODamage/Views/ListClients.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOODamage.BackEnd;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace OOODamage.Views
         public ListClients()
         {
             InitializeComponent();
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            SharedClass.OpenNewPage(this, new AddOrEditClient());
         }
     }
 }
